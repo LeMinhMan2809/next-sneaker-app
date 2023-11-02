88214afcb16de5d55be2f522a4d08ecd831eecca
LaThanhTrong <lathanhtrong322002@gmail.com>
2023-11-02 21:54:35 +0700

    update

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,76 +1,85 @@
 import { useState } from "react"
 import axios from "axios"
 import { useRouter } from "next/router";
+import { ReactSortable } from "react-sortablejs";
 
-export default function ProductForm ({_id, brand: existingBrand, title: existingTitle, description:existingDescription,images}) {
+export default function ProductForm({ _id, brand: existingBrand, title: existingTitle, description: existingDescription, images: existingImages }) {
 
     const [brand, setBrand] = useState(existingBrand || '')
     const [title, setTitle] = useState(existingTitle || '')
     const [description, setDescription] = useState(existingDescription || '')
-    
-    const [goToProducts, setGoToProducts] = useState(false)
-    const router = useRouter() 
-    async function saveProduct (e){
-        e.preventDefault()
-        const data = {brand, title, description}
+    const [images, setImages] = useState(existingImages || [])
 
-        if (_id){
+    const [goToProducts, setGoToProducts] = useState(false)
+    const router = useRouter()
+    async function saveProduct(e) {
+        e.preventDefault()
+        const data = { brand, title, description }
+
+        if (_id) {
             //Update
-            await axios.put ('/api/products/', {...data,_id})
+            await axios.put('/api/products/', { ...data, _id })
         } else {
             //Create
-            const data = {brand, title, description}
+            const data = { brand, title, description }
             await axios.post('/api/products', data)
-            
+
         }
         setGoToProducts(true)
     }
 
-    if (goToProducts){
+    if (goToProducts) {
         router.push('/products')
     }
 
-    async function uploadImages(e){
+    async function uploadImages(e) {
         const files = e.target?.files
-        if (files?.length > 0){
+        if (files?.length > 0) {
             const data = new FormData()
-            for (const file of files){
+            for (const file of files) {
                 data.append('file', file)
             }
-            const res = await fetch('/api/upload', {
-                method: 'POST',
-                body: data
+            const res = await axios.post('/api/upload', data)
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links]
             })
-            console.log(res.data)
         }
+    }
+
+    function updateImagesOrder(images) {
+        setImages(images)
     }
 
     return (
         <form onSubmit={saveProduct} action="" method="POST">
 
             <label>Product brand</label>
-            <input type="text" placeholder="Product brand" value={brand} onChange={e => setBrand(e.target.value)}/>
+            <input type="text" placeholder="Product brand" value={brand} onChange={e => setBrand(e.target.value)} />
 
             <label>Product name</label>
-            <input type="text" placeholder="Product name" value={title} onChange={e => setTitle(e.target.value)}/>
+            <input type="text" placeholder="Product name" value={title} onChange={e => setTitle(e.target.value)} />
 
             <label>Product description</label>
-            <input type="text" placeholder="Product description" value={description} onChange={e => setDescription(e.target.value)}/>
+            <input type="text" placeholder="Product description" value={description} onChange={e => setDescription(e.target.value)} />
 
             <label className="pb-2">Photos</label>
-            <div className="mt-2">
-                <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-md bg-gray-200 ">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
-                            </svg>
-                            
-                            <div>Upload</div>
-                            <input type="file" onChange={uploadImages} className="hidden" />
+            <div className="mb-2 flex flex-wrap gap-1">
+                <ReactSortable list={images} className="flex flex-warp gap-1" setList={updateImagesOrder}>
+                    {!!images?.length && images.map(link => (
+                        <div key={link} className="h-24 bg-white p-2 rounded-sm border border-gray-200 shadow-sm">
+                            <img className="max-h-full rounded-full" src={link} />
+                        </div>
+                    ))}
+                </ReactSortable>
+                <label className="w-24 h-24 text-center flex items-center justify-center gap-1 text-sm text-gray-900 rounded-sm bg-white shadow-sm border border-gray-200 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                    </svg>
+                    <div>
+                        Upload
+                    </div>
+                    <input type="file" onChange={uploadImages} className="hidden"></input>
                 </label>
-            
-
-                {!images?.length && (<div>No images</div>)}
-            
             </div>
 
 
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,16 +1,16 @@
 import multiparty from 'multiparty'
-import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3'
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 const bucketName = 'elle-next-s3'
 
-export default async function handle(req, res){
-    
+export default async function handle(req, res) {
+
     const form = new multiparty.Form()
-    const {fields,files} = await new Promise((resolve, reject) => {
+    const { fields, files } = await new Promise((resolve, reject) => {
         form.parse(req, (err, fields, files) => {
-            if(err){
+            if (err) {
                 reject(err)
             }
-            resolve({fields, files})
+            resolve({ fields, files })
         })
     })
     const client = new S3Client({
@@ -21,18 +21,18 @@ export default async function handle(req, res){
         }
     })
 
-    for(const file of files.file){
+    for (const file of files.file) {
         const ext = file.originalFilename.split('.').pop()
-        console.log({ext, file})
+        console.log({ ext, file })
         const newFilename = Date.now() + '.' + ext
         await client.send(new PutObjectCommand({
             Bucket: bucketName,
             Key: newFilename,
         }))
-        
+
     }
 }
 
 export const config = {
-    api: {bodyParser: false}
+    api: { bodyParser: false }
 }
