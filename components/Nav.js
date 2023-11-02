6721733d78192cb4f36c2a5011c22a53c375b3d1
LaThanhTrong <lathanhtrong322002@gmail.com>
2023-11-03 00:09:09 +0700

    inventory

--- components/Nav.js
+++ components/Nav.js
@@ -2,8 +2,8 @@ import Link from "next/link"
 import { useRouter } from "next/router";
 import { signOut } from "next-auth/react"
 export default function Nav() {
-    const inactiveLink = 'flex gap-1 p-1';
-    const activeLink = inactiveLink + ' bg-white text-green-900 rounded-l-lg ';
+    const inactiveLink = 'flex gap-1 p-1 items-center';
+    const activeLink = inactiveLink + ' bg-white text-green-900';
 
     const router = useRouter();
     const { pathname } = router;
@@ -31,14 +31,31 @@ export default function Nav() {
                     </svg>
                     Home
                 </Link>
-
-                <Link href={'/products'} className={pathname.includes('/products') ? activeLink : inactiveLink}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
-                    </svg>
-                    Product
-                </Link>
-
+                <div className={(pathname.includes('/products') || pathname.includes('/inventory')) ? "bg-white text-green-900" : ""}>
+                    <Link href='/products' className={pathname.includes('/products') ? activeLink : inactiveLink}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                        </svg>
+                        <span>Products</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </Link>
+                    <div className={(pathname.includes('/products') || pathname.includes('/inventory')) ? "block" : "hidden"}>
+                        <Link href='/products' className={(activeLink) + (pathname.includes('/products') ? " text-[15px] pl-7 font-bold" : " text-[15px] pl-7 font-normal")}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="max-w-[30px]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+                            </svg>
+                            <span>Products Details</span>
+                        </Link>
+                        <Link href='/inventory' className={(activeLink) + (pathname.includes('/inventory') ? " text-[15px] pl-7 font-bold" : " text-[15px] pl-7 font-normal")}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="max-w-[30px]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M7.875 14.25l1.214 1.942a2.25 2.25 0 001.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 011.872 1.002l.164.246a2.25 2.25 0 001.872 1.002h2.092a2.25 2.25 0 001.872-1.002l.164-.246A2.25 2.25 0 0116.954 9h4.636M2.41 9a2.25 2.25 0 00-.16.832V12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 01.382-.632l3.285-3.832a2.25 2.25 0 011.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0021.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 002.25 2.25z" />
+                            </svg>
+                            <span>Inventory</span>
+                        </Link>
+                    </div>
+                </div>
                 <Link href={'/categories'} className={pathname.includes('/categories') ? activeLink : inactiveLink}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z" />
---
+++ pages/inventory.js
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+export default function Inventory() {
+    return (
+        <Layout>
+            <div>
+                <h1>Inventory</h1>
+            </div>
+        </Layout>
+    )
+}
