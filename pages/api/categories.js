f65371f01a449bab366897adbba6664dd45f59e0
LaThanhTrong <lathanhtrong322002@gmail.com>
2023-11-07 15:58:00 +0700

    update

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -5,36 +5,34 @@ import { getServerSession } from "next-auth";
 
 
 export default async function handle(req, res) {
-    const {method} = req;
+    const { method } = req;
     await mongooseConnect();
 
     // //Security
     // await isAdminRequest(req, res)
 
-    if(method == 'GET'){
+    if (method == 'GET') {
         res.json(await Category.find().populate('parent'))
     }
 
-    if (method === 'POST'){
-        const {name, parentCategory} = req.body;
-        const categoryDoc = await Category.create({name, parent:parentCategory || undefined})
+    if (method === 'POST') {
+        const { name, parentCategory } = req.body;
+        const categoryDoc = await Category.create({ name, parent: parentCategory || undefined })
         res.json(categoryDoc)
     }
 
-    if (method === 'PUT'){
-        const {name, parentCategory, _id} = req.body;
-
-        if (!mongoose.Types.ObjectId.isValid(parentCategory)) {
-            res.status(400).json({ error: 'Invalid _id value' });
-            return;
-    }
-        const categoryDoc = await Category.updateOne({_id},{name, parent:parentCategory || undefined})
-        res.json (categoryDoc)
+    if (method === 'PUT') {
+        const { name, parentCategory, _id } = req.body;
+        const categoryDoc = await Category.updateOne({ _id }, {
+            name,
+            parent: parentCategory || null,
+        });
+        res.json(categoryDoc);
     }
 
-    if (method === 'DELETE'){
+    if (method === 'DELETE') {
         const { _id } = req.query;
-        await Category.deleteOne({_id})
+        await Category.deleteOne({ _id })
         res.json('ok')
     }
 }
